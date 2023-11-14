9d896a712ed67d51c5f1d01dd93fa98d8fe0ed24
Spencer Lewis <spence.914@gmail.com>
2023-11-14 14:05:42 -0800

    initial commit, create program file, outline scope of program, install required modules to directory

---
+++ fetcher.js
@@ -0,0 +1,11 @@
+// fetcher takes a URL, and a local file path and downloads the resource at the URL to the
+// path on my machine
+
+const fetcher = function(URL, localFilePath) {
+
+};
+
+
+// Upon completion it will print a message like:
+// > node fetcher.js http://www.example.edu/ ./index.html
+// Downloaded and saved 3261 bytes to ./index.html
