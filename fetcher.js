093978961177af862421351eae2946f14fe7624a
Spencer Lewis <spence.914@gmail.com>
2023-11-14 15:16:37 -0800

    add code to request body html from URL entered as CLI argument, and write it to CLI argument for destination, and to print a log of the size of the page downloaded

--- fetcher.js
+++ fetcher.js
@@ -1,10 +1,22 @@
 // fetcher takes a URL, and a local file path and downloads the resource at the URL to the
 // path on my machine
+const URL = process.argv[2];
+const localFilePath = process.argv[3];
+
+const request = require('request');
+const fs = require('fs');
 
 const fetcher = function(URL, localFilePath) {
-
+  request(URL, (error, response, body) => {
+    fs.writeFile(localFilePath, body, (err) => {
+      if (err) {
+        throw err;
+      } console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`);
+    });
+  });
 };
 
+fetcher(URL, localFilePath);
 
 // Upon completion it will print a message like:
 // > node fetcher.js http://www.example.edu/ ./index.html
